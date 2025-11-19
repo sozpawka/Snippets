6276d9512b26a6f3d644c528bc3a4eca2abe9b3c
Sozpawka <sozpawkawar@gmail.com>
2025-11-19 16:10:16 +0700

    1_commit_19.11

---
+++ tutorial/snippets/views.py
@@ -0,0 +1,50 @@
+from rest_framework import generics, permissions, renderers
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+from .models import Snippet
+from .permissions import IsOwnerOrReadOnly
+from .serializers import SnippetSerializer, UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+   return Response({
+       'users': reverse('user-list', request=request, format=format),
+       'snippets': reverse('snippet-list', request=request, format=format)
+   })
+
+
+class UserList(generics.ListAPIView):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+
+class SnippetList(generics.ListCreateAPIView):
+   queryset = Snippet.objects.all()
+   serializer_class = SnippetSerializer
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+   def perform_create(self, serializer):
+       serializer.save(owner=self.request.user)
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+   queryset = Snippet.objects.all()
+   serializer_class = SnippetSerializer
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+class SnippetHighlight(generics.GenericAPIView):
+   queryset = Snippet.objects.all()
+   renderer_classes = [renderers.StaticHTMLRenderer]
+
+   def get(self, request, **kwargs):
+       snippet = self.get_object()
+       return Response(snippet.highlighted)
+
